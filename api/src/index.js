c40e1a70d958ac67d07758c16b1f1453d7062663
SkylexX02 <andrea.dubruel@gmail.com>
2022-11-30 11:21:36 +0100

    commit 3

--- api/src/index.js
+++ api/src/index.js
@@ -2,14 +2,30 @@ import "dotenv/config.js";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { checkJwt } from "./auth/jwt-middleware.js";
+import { postBeep, BeepTooLongError } from "./use-case/post-beep.js";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(checkJwt);
 
 app.get("/", (req, res) => {
-	res.status(200).send("Hello world");
+	res.status(200).send(`Hello ${req.auth.sub}`);
+});
+
+app.post("/beep", async (req, res) => {
+	try {
+		const postedBeep = await postBeep(req.auth.sub, req.body.content);
+		res.status(201).json(postedBeep);
+	} catch (e) {
+		if (e instanceof BeepTooLongError) {
+			res.status(400).send("Beep too long");
+		} else {
+			throw e;
+		}
+	}
 });
 
 app.listen(8080);
